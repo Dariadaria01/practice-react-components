9ab037b28c425c54af157419c552ee6abeef8c95
Daria Seńko <twoj@email>
2025-11-16 18:22:28 +0100

    zadania

--- 03/app.js
+++ 03/app.js
@@ -4,40 +4,64 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
-    state = {
-        comments: [],
-    }
-    
-    render() {
-        const {title, body} = this.props;
-        return (
-            <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-                <section>
-                    <form>
-                        <div>
-                            <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
-                                />
-                            </label>
-                        </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
-                    </form>
-                    <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
-                    </ul>
-                </section>
-            </article>
-        )
-    }
+  state = {
+    comments: [],
+    newComment: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({ newComment: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { newComment, comments } = this.state;
+    if (newComment.trim() === '') return;
+
+    this.setState({
+      comments: [...comments, newComment],
+      newComment: '',
+    });
+  };
+
+  render() {
+    const { title, body } = this.props;
+    const { comments, newComment } = this.state;
+    return (
+      <article>
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <section>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>
+                <textarea
+                  style={{ minWidth: '300px', minHeight: '120px' }}
+                  name='content'
+                  value={newComment}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+            <div>
+              <input type='submit' value='dodaj komentarz' />
+            </div>
+          </form>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment}</li>
+            ))}
+          </ul>
+        </section>
+      </article>
+    );
+  }
 }
 
 root.render(
-    <Article 
-        title="Programowanie jest super!"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />
+  <Article
+    title='Programowanie jest super!'
+    body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam...'
+  />
 );
